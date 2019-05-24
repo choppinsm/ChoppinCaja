73cd69c0cf54802da250c7dde3d7dcc64749482b
sebas <sebastianoscarlopz@gmail.com>
2019-05-23 22:29:56 -0300

    Versión inicial

--- ChoppInCaja/FormVentas.Designer.cs
+++ ChoppInCaja/FormVentas.Designer.cs
@@ -28,78 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
-			this.GridMesas = new System.Windows.Forms.DataGridView();
-			this.label1 = new System.Windows.Forms.Label();
-			this.gridMesaDetalle = new System.Windows.Forms.DataGridView();
-			((System.ComponentModel.ISupportInitialize)(this.GridMesas)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.gridMesaDetalle)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// GridMesas
-			// 
-			this.GridMesas.AllowUserToAddRows = false;
-			this.GridMesas.AllowUserToDeleteRows = false;
-			this.GridMesas.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.GridMesas = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gridMesaDetalle = new System.Windows.Forms.DataGridView();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblEstado = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.btnCerrarMesa = new System.Windows.Forms.Button();
+            this.CboVentas = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.GridMesas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridMesaDetalle)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GridMesas
+            // 
+            this.GridMesas.AllowUserToAddRows = false;
+            this.GridMesas.AllowUserToDeleteRows = false;
+            this.GridMesas.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
-			this.GridMesas.BackgroundColor = System.Drawing.Color.Black;
-			this.GridMesas.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-			this.GridMesas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.GridMesas.GridColor = System.Drawing.Color.Black;
-			this.GridMesas.Location = new System.Drawing.Point(12, 30);
-			this.GridMesas.MultiSelect = false;
-			this.GridMesas.Name = "GridMesas";
-			this.GridMesas.ReadOnly = true;
-			this.GridMesas.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-			this.GridMesas.Size = new System.Drawing.Size(210, 196);
-			this.GridMesas.TabIndex = 0;
-			this.GridMesas.RowEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.GridMesas_RowEnter);
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Arial", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.ForeColor = System.Drawing.Color.White;
-			this.label1.Location = new System.Drawing.Point(12, 9);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(63, 18);
-			this.label1.TabIndex = 1;
-			this.label1.Text = "Mesas:";
-			// 
-			// gridMesaDetalle
-			// 
-			this.gridMesaDetalle.AllowDrop = true;
-			this.gridMesaDetalle.AllowUserToOrderColumns = true;
-			this.gridMesaDetalle.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.GridMesas.BackgroundColor = System.Drawing.Color.Black;
+            this.GridMesas.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.GridMesas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridMesas.GridColor = System.Drawing.Color.Black;
+            this.GridMesas.Location = new System.Drawing.Point(13, 46);
+            this.GridMesas.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.GridMesas.MultiSelect = false;
+            this.GridMesas.Name = "GridMesas";
+            this.GridMesas.ReadOnly = true;
+            this.GridMesas.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.GridMesas.Size = new System.Drawing.Size(394, 686);
+            this.GridMesas.TabIndex = 0;
+            this.GridMesas.DataError += new System.Windows.Forms.DataGridViewDataErrorEventHandler(this.GridMesas_DataError);
+            this.GridMesas.RowEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.GridMesas_RowEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(18, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mesas:";
+            // 
+            // gridMesaDetalle
+            // 
+            this.gridMesaDetalle.AllowDrop = true;
+            this.gridMesaDetalle.AllowUserToOrderColumns = true;
+            this.gridMesaDetalle.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.gridMesaDetalle.BackgroundColor = System.Drawing.Color.Black;
-			this.gridMesaDetalle.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-			this.gridMesaDetalle.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.gridMesaDetalle.GridColor = System.Drawing.Color.Black;
-			this.gridMesaDetalle.Location = new System.Drawing.Point(228, 30);
-			this.gridMesaDetalle.Name = "gridMesaDetalle";
-			this.gridMesaDetalle.Size = new System.Drawing.Size(421, 196);
-			this.gridMesaDetalle.TabIndex = 2;
-			this.gridMesaDetalle.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridMesaDetalle_CellValueChanged);
-			this.gridMesaDetalle.DataError += new System.Windows.Forms.DataGridViewDataErrorEventHandler(this.gridMesaDetalle_DataError);
-			this.gridMesaDetalle.EditingControlShowing += new System.Windows.Forms.DataGridViewEditingControlShowingEventHandler(this.gridMesaDetalle_EditingControlShowing);
-			// 
-			// FormVentas
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.Color.Black;
-			this.ClientSize = new System.Drawing.Size(661, 238);
-			this.Controls.Add(this.gridMesaDetalle);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.GridMesas);
-			this.Name = "FormVentas";
-			this.Text = "Mesas";
-			this.Load += new System.EventHandler(this.Ventas_Load);
-			((System.ComponentModel.ISupportInitialize)(this.GridMesas)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.gridMesaDetalle)).EndInit();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.gridMesaDetalle.BackgroundColor = System.Drawing.Color.Black;
+            this.gridMesaDetalle.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.gridMesaDetalle.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridMesaDetalle.GridColor = System.Drawing.Color.Black;
+            this.gridMesaDetalle.Location = new System.Drawing.Point(415, 46);
+            this.gridMesaDetalle.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gridMesaDetalle.Name = "gridMesaDetalle";
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Wingdings", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.gridMesaDetalle.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.gridMesaDetalle.RowHeadersWidth = 70;
+            this.gridMesaDetalle.RowTemplate.Height = 40;
+            this.gridMesaDetalle.Size = new System.Drawing.Size(825, 636);
+            this.gridMesaDetalle.TabIndex = 2;
+            this.gridMesaDetalle.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridMesaDetalle_CellValueChanged);
+            this.gridMesaDetalle.DataError += new System.Windows.Forms.DataGridViewDataErrorEventHandler(this.gridMesaDetalle_DataError);
+            this.gridMesaDetalle.EditingControlShowing += new System.Windows.Forms.DataGridViewEditingControlShowingEventHandler(this.gridMesaDetalle_EditingControlShowing);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblEstado});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 745);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1258, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.BackColor = System.Drawing.Color.Black;
+            this.lblEstado.ForeColor = System.Drawing.Color.Lime;
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(0, 17);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.ForeColor = System.Drawing.Color.White;
+            this.lblTotal.Location = new System.Drawing.Point(414, 690);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(38, 42);
+            this.lblTotal.TabIndex = 4;
+            this.lblTotal.Text = "$";
+            // 
+            // btnCerrarMesa
+            // 
+            this.btnCerrarMesa.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCerrarMesa.Location = new System.Drawing.Point(1044, 690);
+            this.btnCerrarMesa.Name = "btnCerrarMesa";
+            this.btnCerrarMesa.Size = new System.Drawing.Size(196, 42);
+            this.btnCerrarMesa.TabIndex = 5;
+            this.btnCerrarMesa.Text = "Cerrar Mesa";
+            this.btnCerrarMesa.UseVisualStyleBackColor = true;
+            this.btnCerrarMesa.Click += new System.EventHandler(this.btnCerrarMesa_Click);
+            // 
+            // CboVentas
+            // 
+            this.CboVentas.FormattingEnabled = true;
+            this.CboVentas.Location = new System.Drawing.Point(571, 14);
+            this.CboVentas.Name = "CboVentas";
+            this.CboVentas.Size = new System.Drawing.Size(286, 28);
+            this.CboVentas.TabIndex = 6;
+            this.CboVentas.SelectedIndexChanged += new System.EventHandler(this.CboVentas_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(472, 13);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 28);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Ventas:";
+            // 
+            // FormVentas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaGreen;
+            this.ClientSize = new System.Drawing.Size(1258, 767);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.CboVentas);
+            this.Controls.Add(this.btnCerrarMesa);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.gridMesaDetalle);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.GridMesas);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FormVentas";
+            this.Text = "Ventas";
+            this.Load += new System.EventHandler(this.Ventas_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.GridMesas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridMesaDetalle)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -108,6 +195,12 @@
         private System.Windows.Forms.DataGridView GridMesas;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView gridMesaDetalle;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblEstado;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Button btnCerrarMesa;
+        private System.Windows.Forms.ComboBox CboVentas;
+        private System.Windows.Forms.Label label2;
     }
 }
 
